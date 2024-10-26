d8398d90d53f5c6cabf90f4ade08b7976464136f
Jae-Seung Lee <jaeseung@gmail.com>
2024-10-26 17:25:16 -0400

    chore: Update swift tools version to 5.10

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Persistence",
-    platforms: [.macOS(.v12), .iOS(.v15)],
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v6)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,7 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Persistence",
-            dependencies: []),
+            dependencies: [],
+            swiftSettings: [
+                .enableExperimentalFeature("StrictConcurrency")
+        ]),
         .testTarget(
             name: "PersistenceTests",
             dependencies: ["Persistence"]),
